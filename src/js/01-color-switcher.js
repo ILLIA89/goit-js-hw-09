1ad8fc37f40d9c8a9eb50014bd81abc9ef5dbaa1
ILLIA89 <anolij1@gmail.com>
2023-08-07 21:48:02 +0200

    task-1.1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+// спочатку отримуємо посилання посилання до кнопок через DOM
+
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+let interval;
+
+startBtn.addEventListener("click", ()=>{
+    
+    if(!interval){
+        startBtn.disabled = true;
+        stopBtn.disabled = false;
+    }
+    interval = setInterval(()=>{
+document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+});
+
+stopBtn.addEventListener("click", ()=>{
+    if(interval){
+        clearInterval(interval)
+        interval = null
+        startBtn.disabled = false;
+        stopBtn.disabled = true;
+    }
+})
