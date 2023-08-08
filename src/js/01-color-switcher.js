0fdd9613efd1c4180476c2e3fe258953be2b3aa9
ILLIA89 <anolij1@gmail.com>
2023-08-08 18:42:36 +0200

    task-1.2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -9,20 +9,26 @@ const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 
 let interval;
-
+// додаємо слухача подій на кнопку startBtn
 startBtn.addEventListener("click", ()=>{
-    
+    //  додаємо умову, яка зробить кнопку не активною підчас зміни колоьору
     if(!interval){
         startBtn.disabled = true;
         stopBtn.disabled = false;
     }
+    //  за допомогою setInterval і функції getRandomHexColor змінюємо backgroundColor 
+    // кожну секунду
     interval = setInterval(()=>{
 document.body.style.backgroundColor = getRandomHexColor();
     }, 1000);
 });
 
+// додаємо слухача подій на кнопку stopBtn яка зупиняє перемикання backgroundColor
+// на сторінці
+
 stopBtn.addEventListener("click", ()=>{
-    if(interval){
+    if (interval) {
+        // анулюємо дію setInterval
         clearInterval(interval)
         interval = null
         startBtn.disabled = false;
